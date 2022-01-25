b1e538c9b5d93b6d64063965b80e15ebc000aa1e
Elnur M. Jafarov <elnurmj@code.local>
2022-01-25 14:32:52 +0400

    tasks completed

---
+++ JS Devide to 3-7/assets/script.js
@@ -0,0 +1,13 @@
+let n = 21;
+
+devideTo3And7(n);
+
+function devideTo3And7(n){
+    if (n%3 == 0 && n%7 == 0) {
+        console.log("Number is devided to 3 and 7");
+    }
+    else{
+        console.log("Number is not devided to 3 and 7");
+    }
+}
+alert("Welcome Cavid Bashirov")
---
+++ JS Square even numbers/assets/js/script.js
@@ -0,0 +1,18 @@
+let arr = [1,2,3,4,5,6,7,8,9];
+let sum = 0;
+
+
+numbers(arr);
+
+function numbers(arr){
+
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i]%2 == 0) {
+            sum += arr[i] * arr[i]
+
+        }
+    }
+    console.log(sum);
+}
+
+alert("Welcome Cavid Bashirov")
